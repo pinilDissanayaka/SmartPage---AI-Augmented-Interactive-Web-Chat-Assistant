ffcd4e7229556432ba08447452d71a570eec49cb
pinilDissanayaka <pinildissanayaka@gmail.com>
2024-11-08 00:12:48 +0530

    define embeddings

--- web_page/web_page.py
+++ web_page/web_page.py
@@ -1,4 +1,5 @@
 from langchain.document_loaders import WebBaseLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 
 
@@ -7,3 +8,8 @@ def load_page(urls):
     documents = loader.load()
     
     return documents
+
+def split_page(documents):
+    splitter=RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0, length_function=len)
+    
+    return splitter.split_documents(documents)
