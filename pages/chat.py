d525768984ebabe55d8a302c2434404b964f6a9e
pinilDissanayaka <pinildissanayaka@gmail.com>
2024-11-07 12:23:20 +0530

    secrets

--- pages/chat.py
+++ pages/chat.py
@@ -1,17 +1,23 @@
 import os
 import streamlit as st
+from secret import load_secret
 
 
 st.set_page_config(page_title="SmartPage", page_icon="🤖")
 
 
 with st.sidebar:
-    st.title("Welcome to SmartPage – Your AI-Enhanced Web Chat Assistant 🤖💬")
+    st.title("SmartPage – Your AI-Enhanced Web Chat Assistant 🤖💬")
     
     
-    if not ("GROQ_API_KEY" in st.session_state.keys() and "PINECONE_API_KEY" in st.session_state.keys()):
-        st.session_state["GROQ_API_KEY"] = st.text_input("Enter your Groq API key :", type="password")
+    if not ("GROQ_API_KEY" in st.secrets.keys() and "PINECONE_API_KEY" in st.secrets.keys()):
+        groq_api_key = st.text_input(label="Enter your Groq API key :", value=None, type="password")
         
-        st.session_state["PINECONE_API_KEY"] = st.text_input("Enter your Pinecone API key :", type="password")
-    
-    
+        pinecone_api_key = st.text_input(label="Enter your Pinecone API key :", value=None, type="password")
+
+        if groq_api_key and pinecone_api_key:
+            load_secret(groq_api_key=groq_api_key, pinecone_api_key=pinecone_api_key)
+        else:
+            st.error("Please enter both Groq and Pinecone API keys.", icon="🚨")
+    else:
+        pass
---
+++ secret/secret.py
@@ -0,0 +1,12 @@
+import os
+import streamlit as st
+
+
+def load_secret(groq_api_key=None, pinecone_api_key=None):
+    if groq_api_key and pinecone_api_key:
+        os.environ["GROQ_API_KEY"] = groq_api_key
+        os.environ["PINECONE_API_KEY"] = pinecone_api_key
+    else:
+        os.environ["GROQ_API_KEY"] = st.secrets["GROQ_API_KEY"]
+        os.environ["PINECONE_API_KEY"] = st.secrets["PINECONE_API_KEY"]
+        
