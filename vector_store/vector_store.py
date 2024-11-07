25ab0c9221f0e3e91f632a2b7d438621b11b99ab
pinilDissanayaka <pinildissanayaka@gmail.com>
2024-11-08 00:28:07 +0530

    define db

--- vector_store/vector_store.py
+++ vector_store/vector_store.py
@@ -1,1 +1,15 @@
-from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings 
+
+
+def load_vector_store():
+    model_name = "sentence-transformers/all-mpnet-base-v2"
+    model_kwargs = {'device': 'cpu'}
+    encode_kwargs = {'normalize_embeddings': False}
+    
+    embeddings = HuggingFaceEmbeddings(
+        model_name=model_name,
+        model_kwargs=model_kwargs,
+        encode_kwargs=encode_kwargs
+    )
+    
+    
