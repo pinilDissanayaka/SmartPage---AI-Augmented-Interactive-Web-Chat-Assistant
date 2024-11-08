aaf2e4eb97ecc662e71467dd9de70013b4aedb6a
pinilDissanayaka <pinildissanayaka@gmail.com>
2024-11-08 20:56:16 +0530

    add document clean

--- web_page/web_page.py
+++ web_page/web_page.py
@@ -1,4 +1,5 @@
 from langchain_community.document_loaders import WebBaseLoader
+from langchain_core.documents import Document
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 
@@ -7,7 +8,14 @@ def load_page(urls):
     loader = WebBaseLoader(web_path=urls)
     documents = loader.load()
     
-    return documents
+    cleaned_documents = []
+    
+    for document in documents:
+        page_content=document.page_content.replace("\n", "")
+        meta_data=document.metadata
+        cleaned_documents.append(Document(page_content=page_content, metadata=meta_data))
+    
+    return cleaned_documents
 
 def split_page(documents):
     splitter=RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0, length_function=len)
