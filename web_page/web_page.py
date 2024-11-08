147154d2d4cb1c2d89daf19a1d7bb1a8859e7be2
pinilDissanayaka <pinildissanayaka@gmail.com>
2024-11-08 16:00:16 +0530

    change loader

--- web_page/web_page.py
+++ web_page/web_page.py
@@ -1,4 +1,4 @@
-from langchain.document_loaders import WebBaseLoader
+from langchain_community.document_loaders import WebBaseLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 
